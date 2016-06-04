257acc7ec1b62063a8a401397bb7ace6f798f17a
Carlos Bello <bellopauste@yahoo.com>
2016-06-04 18:17:39 +0200

    Move dependencies from CDN to bower for faster local development

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -3,11 +3,10 @@ requirejs.config({
   baseUrl: '/javascripts/libs',
   paths: {
     app: '/javascripts',
-    jquery: '//kendo.cdn.telerik.com/2016.2.504/js/jquery.min',
-    k: '//kendo.cdn.telerik.com/2016.2.504/js',
-    'jquery.easing': '//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min',
+    jquery: '/bower_components/kendo-ui/js/jquery.min',
+    k: '/bower_components/kendo-ui/js',
     templates: '/templates',
-    lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.9.0/lodash.min'
+    lodash: '/bower_components/lodash/dist/lodash.min'
   },
   shim: {
     k: ['jquery'],
